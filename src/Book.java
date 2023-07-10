f70b30fe07c0199d7edf52adbde41494d774cb00
Old God Flow <writebogdanov@gmail.com>
2023-07-10 13:00:29 +0300

    созданы классы и геттеры

---
+++ src/Book.java
@@ -0,0 +1,23 @@
+public class Book {
+    private String name;
+    private Author author;
+    private int year;
+
+    public Book(String name, Author author, int year) {
+        this.name = name;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
